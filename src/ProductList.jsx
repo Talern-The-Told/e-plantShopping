4eb376bc1f25fd1a0a75d102968da9bb5b9bff44
Tanner <tannerhamilton2000@gmail.com>
2025-02-24 10:02:19 -0800

    build

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -264,12 +264,6 @@ useEffect(() => {
 }, [cart]);
    
 
-
-
-
-
-
-
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -278,16 +272,16 @@ useEffect(() => {
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                <a href="/" style={{textDecoration:'none'}}>
                         <div>
-                    <h3 style={{color:'white'}}>Paradise Nursery</h3>
-                    <i style={{color:'white'}}>Where Green Meets Serenity</i>
+                    <h3 >Paradise Nursery</h3>
+                    <i >Where Green Meets Serenity</i>
                     </div>
                     </a>
                 </div>
               
             </div>
             <div style={styleObjUl}>
-                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>{}{cartQuantity}</h1></a></div>
+                <div> <a  onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                <div> <a  onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg>{}{cartQuantity}</h1></a></div>
             </div>
         </div>
         {!showCart? (
@@ -297,7 +291,7 @@ useEffect(() => {
                     <h1><div>{category.category}</div></h1>
                         <div className="product-list">
                         {category.plants.map((plant, plantIndex) => (
-                        <div className = "product-card" key = {plantIndex}>
+                        <div className = "product-card" key = {plant.name}>
                             <img className = 'product-image' src={plant.image} alt={plant.name}/>
                             <div className='product-title'>{plant.name}</div>
                             
