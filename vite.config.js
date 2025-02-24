5611de89c287d3db4010d8cc1ec8a9611302aa0e
Tanner <tannerhamilton2000@gmail.com>
2025-02-24 11:32:11 -0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/e-plantShopping/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
 });
